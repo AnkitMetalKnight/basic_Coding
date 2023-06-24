0d150b9745a9242f5f18a63804fd5a6aebb70d83
Ankit <starboyinanimetoo@gmail.com>
2023-06-24 22:50:22 +0530

    common elements in 3 arrays

---
+++ common elements in 3 arrays.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            //code here.
+           set<int> st;
+            set<int>s;
+            vector<int>v;
+            for(int i=0;i<n1;i++){
+                st.insert(A[i]);
+            }
+            for(int i=0;i<n2;i++){
+                if(st.find(B[i])!=st.end()){
+                    s.insert(B[i]);
+                    // st.erase(B[i]);
+                }
+            }
+            for(int i=0;i<n3;i++){
+                if(s.find(C[i])!=s.end()){
+                    v.push_back(C[i]);
+                    s.erase(C[i]);
+                }
+            }
+            sort(v.begin(),v.end());
+            return v;
+        }
+
+};
+
+//{ Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}
+// } Driver Code Ends
