a9bad633f8fa09c438e87a4bf3d33fe09cadd0af
Ankit <starboyinanimetoo@gmail.com>
2023-07-01 22:58:49 +0530

    Trapping Rain water problem

---
+++ Trapping Rain water problem.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+        // code here
+        int i=0;
+        int j=0;
+        int sum=0;
+        
+        while(j<n-1)
+        {
+            if(arr[i]>arr[j])
+           { 
+               sum+=(arr[i]-arr[j]);
+           }
+               j++;
+            
+        }
+        return sum;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
